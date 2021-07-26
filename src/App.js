d03e6cf10629aad18c91521a6075429cf85d7615
Emanuele Parlascino <emapage95@gmail.com>
2021-07-26 12:24:51 +0100

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GameOfLife from './GameOfLife';
 
 function App() {
   return (
@@ -7,7 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        3YmRqiRp
+        EIAB2UZQ6PNYWUPVEM4WU6S
+          My projects notes:
+          1 consume API - spotify tiny spotlight
+          pure front-end - roberta's site
+          blog - to be started
+          some fullstack project - maybe hotel idea from reddit post?
+          link to post: https://www.reddit.com/r/WebdevTutorials/comments/ok8p9g/4_javascript_projects_to_build_fast_and_get_hired/
+          palette https://coolors.co/17bf1b-243010-404e7c-96897b-dae2df
         </p>
         <a
           className="App-link"
@@ -18,6 +27,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <GameOfLife/>
     </div>
   );
 }
