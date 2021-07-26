a3e6f4cf38736ef3d907ec6c8222a58b59ccb6ea
Emanuele Parlascino <emapage95@gmail.com>
2021-07-26 18:39:22 +0100

    implemented game of life for background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 import GameOfLife from './GameOfLife';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        3YmRqiRp
-        EIAB2UZQ6PNYWUPVEM4WU6S
+  
+      <GameOfLife/>
+    </div>
+  );
+}
+
+export default App;
+
+/*  
+
+        
           My projects notes:
           1 consume API - spotify tiny spotlight
           pure front-end - roberta's site
@@ -17,19 +24,10 @@ function App() {
           some fullstack project - maybe hotel idea from reddit post?
           link to post: https://www.reddit.com/r/WebdevTutorials/comments/ok8p9g/4_javascript_projects_to_build_fast_and_get_hired/
           palette https://coolors.co/17bf1b-243010-404e7c-96897b-dae2df
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <GameOfLife/>
-    </div>
-  );
-}
+*/
 
-export default App;
+/*
+express vpn credential for some reason
+3YmRqiRp
+EIAB2UZQ6PNYWUPVEM4WU6S
+*/
