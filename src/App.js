862bccce24478083ebb88b5ad21bbfa1a463de29
Emanuele Parlascino <emapage95@gmail.com>
2022-04-13 12:22:11 +0100

    cleaned up comments

--- src/App.js
+++ src/App.js
@@ -42,27 +42,5 @@ function App() {
 
 export default App;
 
-/*  
 
-        
-          My projects notes:
-          1 consume API - spotify tiny spotlight
-          pure front-end - roberta's site
-          blog - to be started
-          some fullstack project - maybe hotel idea from reddit post?
-          link to post: https://www.reddit.com/r/WebdevTutorials/comments/ok8p9g/4_javascript_projects_to_build_fast_and_get_hired/
-          palette https://coolors.co/17bf1b-243010-404e7c-96897b-dae2df
-*/
 
-/*
-express vpn credential for some reason
-3YmRqiRp
-EIAB2UZQ6PNYWUPVEM4WU6S
-
-follow the cursor with a menu to open projects
-https://codepen.io/MarioGiannini/pen/Mrmwdj
-
-nav bar idea: https://codepen.io/grohit/pen/jObGzdG
-
-name wolf niffy -inflow
-*/
